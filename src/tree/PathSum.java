003dc39fe9cfe9f093eb1e67a2c0ffc5cfa7bdce
luojunjie <luojunjie9494@163.com>
2021-03-24 22:23:01 +0800

    完善树的路径和。双dfs+前缀和求解

--- src/tree/PathSum.java
+++ src/tree/PathSum.java
@@ -31,55 +31,40 @@ public class PathSum {
     /**
      * 题目分析：
      * 题目要求我们找的是满足条件的路径，所以必然涉及树的遍历操作。而且要求路径是从根向下的情况，所以不能采用层序、中序、和后续遍历。应该采用自顶向下并按照子树遍历的前序遍历进行操作。
-     * 遍历过程中，如果路径加起来的值等于目标值，那么count就+1，如果已经大于了，就换一条路径。小于就可以继续向下。到底之后需要清空路径信息。
+     * 遍历过程中，没到一个节点，我们就
      * 解题步骤：
      * 1.定义个全局计数器，记录可能清空
      * 2.前序遍历树，记录路径值。
      * 3.如果值等于目标值，计数器+1，如果大于就换路径，如果小于就继续向下。
      * 4.到底后清空路径
+     *
+     * 其实就是对每个节点进行dfs（前中后序遍历都属于是dfs）
      */
-    private int count;
-
-    private int[] route;
-
-    private int index;
+    private int ret = 0;
 
     public int pathSum(TreeNode root, int sum) {
-        index = 0;
-        route = new int[10000];
-        checkSum(root,sum);
-        pathSum(root.left,sum);
-        pathSum(root.right,sum);
-        return count;
+        dfs(root, sum);
+        if (root != null) {
+            pathSum(root.left, sum);
+            pathSum(root.right, sum);
+        }
+        return ret;
     }
 
-    public void checkSum(TreeNode node,int sum){
-        if (node==null){
+    /**
+     * 从root结点出发，求路径和等于sum路径数
+     *
+     * @param root
+     * @param sum
+     */
+    private void dfs(TreeNode root, int sum) {
+        if (root == null) {
             return;
         }
-        route[index++] = node.val;
-        if (sum()==sum){
-            ++count;
-            return;
+        if (root.val == sum) {
+            ret++;
         }
-        if (sum()<sum){
-            checkSum(node.left,sum);
-            route[--index<0?0:index] = 0;
-            checkSum(node.right,sum);
-            route[--index<0?0:index] = 0;
-        }
-    }
-
-    private int sum(){
-        int sum = 0;
-        for (int i:route){
-            sum+=i;
-        }
-        return sum;
-    }
-
-    public static void main(String[] args) {
-        int a= 5;
-        System.out.println(--a < 0 ? 0 : a);
+        dfs(root.left, sum - root.val);
+        dfs(root.right, sum - root.val);
     }
 }
