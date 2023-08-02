25a7d63c4fae7804913fcb4bb81612129ef5ad58
luojunjie <luojunjie9494@163.com>
2023-08-02 20:59:54 +0800

    找字符串的不同

---
+++ src/string/FindTheDifference.java
@@ -0,0 +1,25 @@
+package string;
+
+/**
+ * 找不同
+ * 给定两个字符串 s 和 t ，它们只包含小写字母。
+ *
+ * 字符串 t 由字符串 s 随机重排，然后在随机位置添加一个字母。
+ *
+ * 请找出在 t 中被添加的字母。
+ */
+public class FindTheDifference {
+    /**
+     * 求和取差值，差值就是添加的字符的ASCII 码值---字符操作要多考虑录用ASCII 码值进行操作
+     */
+    public char findTheDifference(String s, String t) {
+        int as = 0, at = 0;
+        for (int i = 0; i < s.length(); ++i) {
+            as += s.charAt(i);
+        }
+        for (int i = 0; i < t.length(); ++i) {
+            at += t.charAt(i);
+        }
+        return (char) (at - as);
+    }
+}
