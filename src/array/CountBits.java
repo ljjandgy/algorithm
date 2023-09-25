c6f81fbc062c6c6de5bcd4403ae74b32446e663e
luojunjie <luojunjie9494@163.com>
2023-09-25 21:54:21 +0800

    新增比特位计数

---
+++ src/array/CountBits.java
@@ -0,0 +1,26 @@
+package array;
+
+/**
+ * 比特位计数
+ */
+public class CountBits {
+    /**
+     * 利用不断把末位变成0来计算有多少个1
+     */
+    public int[] countBits(int n) {
+        int[] bits = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            bits[i] = countOnes(i);
+        }
+        return bits;
+    }
+
+    public int countOnes(int x) {
+        int ones = 0;
+        while (x > 0) {
+            x &= (x - 1);
+            ones++;
+        }
+        return ones;
+    }
+}
