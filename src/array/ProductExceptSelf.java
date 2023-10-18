5d87cd5aab701f428ac6111b76651f2ef8ae1201
luojunjie <luojunjie9494@163.com>
2023-10-18 22:08:17 +0800

    新增求乘积

---
+++ src/array/ProductExceptSelf.java
@@ -0,0 +1,41 @@
+package array;
+
+public class ProductExceptSelf {
+    /**
+     * 左右乘积法求乘积信息
+     * 1.通过两个数组存储某个索引i左右两边的乘积信息
+     * 加入i为3
+     * 则L[3] = nums[1]*nums[2]
+     * R[3] = num[4]*...nums[i-1]
+     */
+    public int[] productExceptSelf(int[] nums) {
+        int length = nums.length;
+
+        // L 和 R 分别表示左右两侧的乘积列表
+        int[] L = new int[length];
+        int[] R = new int[length];
+
+        int[] answer = new int[length];
+
+        // L[i] 为索引 i 左侧所有元素的乘积
+        // 对于索引为 '0' 的元素，因为左侧没有元素，所以 L[0] = 1
+        L[0] = 1;
+        for (int i = 1; i < length; i++) {
+            L[i] = nums[i - 1] * L[i - 1];
+        }
+
+        // R[i] 为索引 i 右侧所有元素的乘积
+        // 对于索引为 'length-1' 的元素，因为右侧没有元素，所以 R[length-1] = 1
+        R[length - 1] = 1;
+        for (int i = length - 2; i >= 0; i--) {
+            R[i] = nums[i + 1] * R[i + 1];
+        }
+
+        // 对于索引 i，除 nums[i] 之外其余各元素的乘积就是左侧所有元素的乘积乘以右侧所有元素的乘积
+        for (int i = 0; i < length; i++) {
+            answer[i] = L[i] * R[i];
+        }
+
+        return answer;
+    }
+}
